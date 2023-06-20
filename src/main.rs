a29704e66317f686735403cd28453bfadab097f7
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2023-06-20 17:27:10 +0900

    create simple prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{stdin, stdout, Write};
+
+
+fn prompt(name: &str) -> String {
+    print!("{}", name);
+    stdout().flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    return input.trim().to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = prompt("db > ");
+    println!("{}", input);
 }
